15674b6ba2fae439144e94b36181d4e80ceed16e
Eugene <berezovskievgenisergeevich@mail.ru>
2024-04-17 23:34:42 +0300

    refactor delete imports

--- src/test/java/web/test/BaseTest.java
+++ src/test/java/web/test/BaseTest.java
@@ -1,13 +1,10 @@
 package web.test;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import config.TestsConfig;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import web.data.TestData;
 
